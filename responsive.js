fdf5ec28365ec5bd929ffffa348cd80a3b65bf70
DIwash Pandey <diwash.pandey.only@gmail.com>
2024-01-11 22:36:51 +0545

    Added theme Mode(Unfinished Colors)
    Theme Changing Mode has been added but colors are unfinished right now. Have to work on it.

--- responsive.js
+++ responsive.js
@@ -1,9 +1,31 @@
 const box1 = document.querySelector(".box-1")
 const revealer = document.querySelector(".revealer")
 
+const modeBox = document.querySelector(".modebox")
+const sunIcon = document.querySelector(".sun-icon")
+const moonIcon = document.querySelector(".moon-icon")
+
 
 function revealBox1() {
     console.log("clicked")
     revealer.classList.toggle("on-reveal")
     box1.classList.toggle("box-1-revealed")
 }
+
+function changeTheme(){
+    if(moonIcon.classList.contains('theme-active')){
+        document.body.classList.add('light_active')
+        moonIcon.classList.remove('theme-active')
+        sunIcon.classList.add('theme-active')
+    }
+    else if(sunIcon.classList.contains('theme-active')){
+        document.body.classList.remove('light_active')
+        sunIcon.classList.remove('theme-active')
+        moonIcon.classList.add('theme-active')
+    }
+    else{
+        alert("Some error has occured :(")
+    }
+}
+
+modeBox.addEventListener('click', changeTheme)
